c6d41706f340af82e297b539bd0660c76c8993c8
muaztalhaBulut <muaztalha.bulut@gappze.com>
2025-07-14 16:05:43 +0300

    Update Package.swift

--- CommonComponents/Package.swift
+++ CommonComponents/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/gappzeTalhaBulut/BaseFoundation.git", from: "1.0.0")
+        .package(url: "https://github.com/gappzeTalhaBulut/BaseFoundation.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
